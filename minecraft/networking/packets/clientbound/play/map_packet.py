7a542ecc04cc7ef5f057dfe47d0308b3b38d936c
Anonymous941 <36797492+Anonymous941@users.noreply.github.com>
2022-07-10 06:17:18 -0700

    Start adding 1.19 map packets

--- minecraft/networking/packets/clientbound/play/map_packet.py
+++ minecraft/networking/packets/clientbound/play/map_packet.py
@@ -9,7 +9,8 @@ from minecraft.networking.types import (
 class MapPacket(Packet):
     @staticmethod
     def get_id(context):
-        return 0x27 if context.protocol_later_eq(755) else \
+        return 0x24 if context.protocol_later_eq(759) else \
+               0x27 if context.protocol_later_eq(755) else \
                0x25 if context.protocol_later_eq(741) else \
                0x26 if context.protocol_later_eq(721) else \
                0x27 if context.protocol_later_eq(550) else \
@@ -36,14 +37,27 @@ class MapPacket(Packet):
             return 'bytearray(...)'
         return super(MapPacket, self).field_string(field)
 
-    class MapIcon(MutableRecord):
-        __slots__ = 'type', 'direction', 'location', 'display_name'
+     if context.protocol_later_eq(759):
+        class MapIcon(MutableRecord):
+            __slots__ = 'type', 'direction', 'location', 'display_name'
 
-        def __init__(self, type, direction, location, display_name=None):
-            self.type = type
-            self.direction = direction
-            self.location = location
-            self.display_name = display_name
+            def __init__(self, type, direction, location, display_name=None):
+                self.type = type
+                self.direction = direction
+                self.location = location
+                self.display_name = display_name
+     else:
+        class MapIcon(MutableRecord):
+            __slots__ = 'type', 'x', 'z', 'direction', 'location', 'has_display_name', 'display_name'
+
+            def __init__(self, type, x, z, direction, location, has_display_name, display_name=None):
+                self.type = type
+                self.x = x
+                self.z = z
+                self.direction = direction
+                self.location = location
+                self.has_display_name
+                self.display_name = display_name
 
     class Map(MutableRecord):
         __slots__ = ('id', 'scale', 'icons', 'pixels', 'width', 'height',
